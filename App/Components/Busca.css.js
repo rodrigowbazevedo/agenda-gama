47b8eec114cb1eb6960c0fe1d8d48b94a930e8a2
Rodrigo Azevedo <rodrigowbazevedo@yahoo.com.br>
2017-02-13 21:16:52 -0200

    Campo de busca

--- App/Components/Busca.css.js
+++ App/Components/Busca.css.js
@@ -2,13 +2,8 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: 'white',
-    marginTop: 55
-  },
-  list: {
     alignSelf: 'stretch'
-  },
+  }
 });
 
 export default styles;
