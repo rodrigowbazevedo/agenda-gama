2256e325f5c0895b3185c961a0a365123814e115
Adolfo Peccin <adolfopeccin@gmail.com>
2017-02-13 21:31:28 -0200

    Melhorias iOS

--- index.ios.js
+++ index.ios.js
@@ -1,16 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry } from 'react-native';
 import Main from './App/Components/Main';
 
 AppRegistry.registerComponent('AgendaGama', () => Main);
